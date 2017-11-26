c479c44fb5ca89d11370f80d84108a8671c4a1e6
wws.hithub <33957175+dlmuwws@users.noreply.github.com>
2017-11-26 20:41:16 +0800

    Update point.h

--- point.h
+++ point.h
@@ -1,1 +1,28 @@
 
+#include<vector>
+using namespace std;
+
+
+
+class point
+{
+private:
+	int x;
+	int y;    
+	int index;   //点的标号
+	int state;  //访问状态
+	vector <point*>cangotopoints_;  
+	vector <point*>adjcentpoints_;
+
+public:
+	point();
+	int get_x() { return x; };
+	int get_y() { return y; };
+	int getindex(int x, int y);
+	void setstate(int a) { state = a;}
+	int getstate() { return state;}
+	void set_pointlist(vector<point *>points);
+	vector<point> get_adjcentpoints();
+	void pushCanGoToBlock(point*point);
+	vector<point*> getCangoTopoints() { return cangotopoints_;}
+};
