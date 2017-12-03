0f327d2d366166dc23445c19a9c211b695a5ae40
Huang-Le <33997857+Huang-Le@users.noreply.github.com>
2017-12-03 17:13:06 +0800

    03/12 main.cpp

---
+++ Execution.cpp
@@ -0,0 +1,18 @@
+#include"DP.h"
+#include"ModelAnalysis.h"
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main() {
+	//getError();
+    //getMAX();
+	//getMIN();
+	//getFeasibleInterval();
+	//getInfeasibleInterval();
+	//ModelAnalysis();
+	//mergeFileDays();
+	ALLModelAnalysis();
+	cout <<'\n'<< "Data Processing finished!" << endl;
+	system("pause");
+}
