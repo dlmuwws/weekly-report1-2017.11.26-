061d82cd94a8224bb271cc6d7327ebe0de4c41fd
wws.hithub <33957175+dlmuwws@users.noreply.github.com>
2017-12-03 20:02:09 +0800

    Update point.h

--- point.h
+++ point.h
@@ -1,28 +1,34 @@
 
 #include<vector>
-using namespace std;
+#include<array>
+#include<stdio.h>
+using namespace std; 
 
 
 
-class point
+class point              // 每个点的基本信息：位置，编号，访问状态，时间—风速，相邻点(有连接的点)
 {
 private:
 	int x;
 	int y;    
 	int index;   //点的标号
 	int state;  //访问状态
-	vector <point*>cangotopoints_;  
-	vector <point*>adjcentpoints_;
+	array<int,18> wind;      //(3:00-21:00)
+	//vector <point>cangotopoints_;  
+	vector <point>adjcentpoints_;
+	pair<int, int>time_wind_;
 
-public:
+public:                             
 	point();
 	int get_x() { return x; };
 	int get_y() { return y; };
 	int getindex(int x, int y);
+	int get_wind_(point*point_);
+	int time_wind_.first();
 	void setstate(int a) { state = a;}
 	int getstate() { return state;}
-	void set_pointlist(vector<point *>points);
 	vector<point> get_adjcentpoints();
-	void pushCanGoToBlock(point*point);
-	vector<point*> getCangoTopoints() { return cangotopoints_;}
+
+
 };
+
