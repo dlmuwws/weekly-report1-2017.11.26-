a758b0cb873e8d25d426cff9edcb9543737ae5e6
wws.hithub <33957175+dlmuwws@users.noreply.github.com>
2017-12-25 20:33:20 +0800

    simplex method (improved)

--- simplex method.cpp
+++ simplex method.cpp
@@ -18,7 +18,7 @@ vector<double>c(m,0);                    //目标函数系数
 vector<double>cb(n,0);
 typedef vector<double>row;
 vector<row>A;                         //系数矩阵
-vector<double>b;
+vector<double>b;   
 
 vector<var*>vars;
 vector<var*> varB;   //基变量
@@ -28,6 +28,7 @@ vector<row>B;          //基
 vector<row>Binverse;
 vector<row>	B_add;           //求Binverse的增广阵
 
+vector<double>reduced_costs;
 
 bool cmp1(double a, double b)
 {
@@ -56,59 +57,27 @@ vector<double>get_cb(vector<var*>varB, vector<double>c)          //确定cb
 	return cb;
 }
 
-vector<row> get_B(vector<var*> varB, vector<row>A)           //确定B
+vector<row> get_B(vector<var*> &varB, vector<row>&A)           //确定B
 {
-	row row1(n,0); row row2(n,0); row row3(n,0);
+	//row row1(n,0); row row2(n,0); row row3(n,0);
 	for (int i=0;i<n;i++ )
 	{
 		for (int j=0;j<m;j++)
 		{
-			if (j == varB[i]->index - 1)
+			if (j == varB[i]->index-1)
 			{
-				row1[i] = A[0][j];
-				row2[i] = A[1][j];
-				row3[i] = A[2][j];
+				B[0][i] = A[0][j];
+				B[1][i] = A[1][j];
+				B[2][i] = A[2][j];
 			}
-			else
-				continue;
 		}
-	}
-	B.push_back(row1); B.push_back(row2); B.push_back(row3);
-	cout << "B:" << endl;
-	for (int i=0;i<n;i++)
-	{
-		for (int j = 0; j<n; j++)
-		{
-			cout << B[i][j]<<"  " ;
-		}
-		cout << endl;
-	}
+	}    
 	return B;
-}
-
-void temp(row row1, row row2, int n)  //交换两行
-{
-	double temp1;
-	for (int i = 0; i<n; i++)
-	{
-		temp1 = row1[i];
-		row2[i] = row2[i];
-		row2[i] = temp1;
-	}
 }
 
 vector<row> get_Binverse(vector<row>&B)                    //求B的逆矩阵
 {
-	row row1; row row2; row row3;
-	for (int i=0;i<2*n;i++)
-	{
-		row1.push_back(0);
-		row2.push_back(0);
-		row3.push_back(0);
-	}
-	B_add.push_back(row1);
-	B_add.push_back(row2);
-	B_add.push_back(row3);
+
 	for (int i=0;i<n;i++)
 	{
 		for (int j= 0; j<n; j++)
@@ -129,7 +98,16 @@ vector<row> get_Binverse(vector<row>&B)                    //求B的逆矩阵
 			for (int j = i; j < n; j++)
 			{
 				if (B_add[j][i] != 0)
-					temp(B_add[i], B_add[j], 2 * n);  //交换两行
+				{
+					//交换ij行
+					double temp;
+					for (int p = 0; p<n * 2; p++)
+					{
+						temp = B_add[j][p];
+						B_add[j][p] = B_add[i][p];
+						B_add[i][p] = temp;
+					}
+				}
 			}
 		}
 		else
@@ -147,7 +125,7 @@ vector<row> get_Binverse(vector<row>&B)                    //求B的逆矩阵
 	}
 
 	double k;
-	for (int i = 0; i<n; i++)        //左矩阵的对角线 全部 转化为  1
+	for (int i = 0; i<n; i++)        //左矩阵的对角线 全部 转化为  1  
 	{
 		k = B_add[i][i];
 		for (int j = 0; j<2 * n; j++)
@@ -163,17 +141,6 @@ vector<row> get_Binverse(vector<row>&B)                    //求B的逆矩阵
 				B_add[j][u] = B_add[j][u] - k*B_add[i][u];
 		}
 	}
-
-	row row4; row row5; row row6;
-	for (int i = 0; i<n; i++)
-	{
-		row4.push_back(0);
-		row5.push_back(0);
-		row6.push_back(0);
-	}
-	Binverse.push_back(row4);
-	Binverse.push_back(row5);
-	Binverse.push_back(row6);
 
 	for (int i = 0; i < n; i++)
 	{
@@ -201,7 +168,7 @@ double get_reduced_cost(var*&var_,vector<double>cb, vector<double>c,vector<row>&
 	double reduced_cost=0;
 	double z = 0;
 	vector<double>cb_mti_Binverse(n,0);             //1*3
-
+	
 	for (int j=0;j<n;j++)
 	{
 		for (int i = 0; i < n; i++)
@@ -211,7 +178,7 @@ double get_reduced_cost(var*&var_,vector<double>cb, vector<double>c,vector<row>&
 	}
 	for (int i=0;i<n;i++)
 	{
-		z += cb_mti_Binverse[i] * A[i][var_->index - 1];
+		z += cb_mti_Binverse[i] * A[i][var_->index - 1];             
 	}
 
 	reduced_cost = z - c[var_->index - 1];
@@ -222,8 +189,9 @@ double get_reduced_cost(var*&var_,vector<double>cb, vector<double>c,vector<row>&
 var* dcid_enter_var(vector<var*> &varN, vector<double>cb, vector<double>c, vector<row>Binverse, vector<row>A)         //确定进基变量
 {
 	var*var_=varN[0];   //进基变量
-	vector<double>reduced_costs;
+	//vector<double>reduced_costs;
 	//int index;   //进基变量的下标
+	//reduced_costs.clear();
 	for (int i=0;i<m-n;i++)
 	{
 		reduced_costs.push_back(get_reduced_cost(varN[i], cb, c,Binverse, A));
@@ -258,7 +226,7 @@ var* dcid_leav_var(var*varE_,vector<double>b, vector<row>Binverse, vector<row>A,
 	vector<double>b_(n,0);
 	vector<double>y_(n,0);
 	for (int j= 0; j<n; j++)
-	{
+	{ 
 		for (int i = 0; i < n; i++)
 		{
 			b_[j] += Binverse[j][i] * b[i];
@@ -267,38 +235,47 @@ var* dcid_leav_var(var*varE_,vector<double>b, vector<row>Binverse, vector<row>A,
 	}
 	vector<double>ratio;
 	vector<int>index;
+	int q = 0;
 	for (int i=0;i<n;i++)
 	{
 		if (y_[i]>0)
 		{
+			q++;
 			ratio.push_back(b_[i] / y_[i]);
 			index.push_back(i);
 		}
 	}
-	//sort(ratio.begin(),ratio.end(),cmp2);
-	int size = ratio.size();
-	if (size > 1)
+	if (q==0)
 	{
-		for (int i = 0; i < size - 1; i++)
+		cout << "find unbounded soln !!!!!" << endl;
+		var_ = NULL;
+	}
+	else
+	{
+		int size = ratio.size();
+		if (size > 1)
 		{
-			for (int j = i + 1; j < size; j++)
+			for (int i = 0; i < size - 1; i++)
 			{
-				if (ratio[i] > ratio[j])
+				for (int j = i + 1; j < size; j++)
 				{
-					double a = ratio[i]; int b = index[i];
-					ratio[i] = ratio[j]; index[i] = index[j];
-					ratio[j] = a; index[j] = b;
+					if (ratio[i] > ratio[j])
+					{
+						double a = ratio[i]; int b = index[i];
+						ratio[i] = ratio[j]; index[i] = index[j];
+						ratio[j] = a; index[j] = b;
+					}
 				}
 			}
 		}
+		int p = ratio.size();
+		cout << "ratio:" << endl;
+		for (int i = 0; i < p; i++)
+		{
+			cout << ratio[i] << "  ";
+		}
+		var_ = varB[index[0]];
 	}
-	int p = ratio.size();
-	cout << "ratio:" << endl;
-	for (int i=0;i<p;i++)
-	{
-		cout << ratio[i] << "  ";
-	}
-	var_ = varB[index[0]];
 	return var_;
 }
 
@@ -317,7 +294,7 @@ vector<var*> changeB(var* var1, var*var2)             //进基出基操作,var1�
 			it++;
 	}
 	varB.push_back(var1);                //var1进基
-	cout <<"test:"<< varB[2]->index << endl;
+	//cout <<"test:"<< varB[2]->index << endl;
 	it = varN.begin();
 	for (it; it != varN.end();)
 	{
@@ -373,15 +350,41 @@ void set_varB(vector<row>Binverse, vector<double>b)
 }
 
 
-void solve(int &d,vector<var*> &varB,vector<double>c, vector<row>A, vector<double>b, vector<var*>& varN, vector<var*> vars)
+int solve(int &d,vector<var*> &varB,vector<double>c, vector<row>A, vector<double>b, vector<var*>& varN, vector<var*> vars)
 {
 	bool a = true;
 	while (a)
 	{
 		cout <<"start the " <<d<<"th" << "  iteration" << endl;
-		B = get_B(varB, A);
+		B.clear(); B_add.clear(); Binverse.clear(); reduced_costs.clear();    //每次迭代都把上次的B和检验数等重置
+		row row4; row row5; row row6;
+		for (int i = 0; i<n; i++)
+		{
+			row4.push_back(0);
+			row5.push_back(0);
+			row6.push_back(0);
+		}
+		Binverse.push_back(row4);
+		Binverse.push_back(row5);
+		Binverse.push_back(row6);
+
+		B.push_back(row4);
+		B.push_back(row5);
+		B.push_back(row6);
+		row row7(2 * n, 0); row row8(2 * n, 0); row row9(2 * n, 0);
+		B_add.push_back(row7);  	B_add.push_back(row8);  	B_add.push_back(row9);
+		//B = get_B(varB, A);
+		cout << "B:" << endl;
+		for (int i = 0; i<n; i++)
+		{
+			for (int j = 0; j<n; j++)
+			{
+				cout << get_B(varB, A)[i][j] << "  ";
+			}
+			cout << endl;
+		}
 		cb = get_cb(varB, c);
-		Binverse = get_Binverse(B);
+		Binverse = get_Binverse(get_B(varB, A));
 		set_varB(Binverse, b);
 		cout << "varB:" << endl;
 		for (int i=0;i<n;i++)
@@ -407,26 +410,58 @@ void solve(int &d,vector<var*> &varB,vector<double>c, vector<row>A, vector<doubl
 		cout << endl;
 		cout << "finish the feasiblity check" << endl;
 		//%%%%%%停止迭代条件 %%%%%
-		int k = 0;
+		int k = 0;  int f = 0;
 		for (int i=0;i<m-n;i++)
 		{
 			if (get_reduced_cost(varN[i],cb,c,Binverse, A)<=0)
 			{
 				//cout << "the reduced costs of var" << varN[i]->index <<"is  "<< get_reduced_cost(varN[i], cb,c, Binverse, A) << "  " << endl;
 				k++;
+				if (get_reduced_cost(varN[i], cb, c, Binverse, A) == 0)
+				{
+					f++;
+				}
 			}
 		}
 		if (k==m-n)
 		{
+			if (f>0)
+			{
+				cout << "there exists alternative opt soln!!!!!" << endl;
+			}
+			else
+			{
+				cout << "all reduced_cost are negative:" << endl;
+			}
+				for (int i = 0; i < m - n; i++)
+				{
+					cout << "reduced cost of var" << varN[i]->index << "  is " << get_reduced_cost(varN[i], cb, c, Binverse, A) << endl;
+				}
+				cout << "having got the opt soln, stop iteration" << endl;
+				cout << "the opt soln is:" << endl;
+				for (int i = 0; i < n; i++)
+				{
+					cout << "var" << varB[i]->index << " = " << varB[i]->data << "  ";
+				}
+			a = false;
 			break;
-			cout << "stop iteration" << endl;
 		}
 		//if haven't got the opt soln:
 		var*var1 = dcid_enter_var(varN, cb, c,Binverse, A);
 		cout << "var" << var1->index << " enter the basis" << endl;
 		var*var2 = dcid_leav_var(var1, b, Binverse, A, varB);
+		if (var2==NULL)
+		{
+			a = false;
+			break;
+		}
 		cout << endl;
 		cout << "var" << var2->index << " leave the basis" << endl;
+		for (int i=0;i<n;i++)
+		{
+			varB[i]->data = 0;
+		}
+
 		vector<var*>newvarB=changeB(var1, var2);             //得到新的基变量
 		cout << "the new varB:" << endl;
 		for (int i=0;i<n;i++)
@@ -441,16 +476,15 @@ void solve(int &d,vector<var*> &varB,vector<double>c, vector<row>A, vector<doubl
 		}
 		cout << "finish the " << d << "th" << "  iteration" << endl;
 		int h=d+1;
-		solve(h,newvarB,c,A,b,newvarN,vars);
+		a=solve(h,newvarB,c,A,b,newvarN,vars);    
 	}
+	return a;
 }
 
 
 int main()
 {
-	cout << "start!!!!" << endl;
-	//m = 6; n = 3;
-	cout << "initialize!!!!" << endl;
+	cout << "start!!!!" << endl; 
 	for (int i=0;i<m;i++)
 	{
 		c.push_back(0);
@@ -461,26 +495,35 @@ int main()
 	row row1(6, 0);    row1[0] = 1; row1[1] = 1; row1[2] = 2; row1[3] = 1;
 	row row2(6, 0);     row2[0] = 1; row2[1] = -1; row2[4] = 1;
 	row row3(6, 0);      row3[1] = -1; row3[2] = 1; row3[5] = 1;
-
+	
 	A.push_back(row1);
 	A.push_back(row2);
 	A.push_back(row3);
 
-	var*var1=new var();  var*var2=new var(); var*var3=new var(); var*var4=new var(); var*var5=new var(); var*var6=new var();
-	//var1->index = 1; var1->data = 0;   var2->index = 2; var2->data = 0;  var3->index = 3; var3->data = 0;
-	//var4->index = 4; var4->data = 0;   var5->index = 5; var5->data = 0;  var6->index = 6; var6->data = 0;
-	//for (int i = 0; i<m; i++)
-//	{
-		//vars[i]->index = i + 1;
-		//vars[i]->data = 0;
-	//}
-	vars.push_back(var1); vars.push_back(var2); vars.push_back(var3); vars.push_back(var4); vars.push_back(var5); vars.push_back(var6);
-	for (int i = 0; i<m; i++)
+	cout << "there are " << m << " vars" << " and " << n << " constraints" << endl;
+	cout << "c:" << endl;
+	for (int i=0;i<m;i++)
+	{
+		cout << "c" << i+1<< "=" << c[i] << ",";
+	}
+	cout << endl;
+	cout << "A:" << endl;
+	for (int i=0;i<n;i++)
+	{
+		for (int j=0;j<m;j++)
+		{
+			cout << A[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	    var*var1=new var();  var*var2=new var(); var*var3=new var(); var*var4=new var(); var*var5=new var(); var*var6=new var();
+	    vars.push_back(var1); vars.push_back(var2); vars.push_back(var3); vars.push_back(var4); vars.push_back(var5); vars.push_back(var6);
+	    for (int i = 0; i<m; i++)
 		{
 		vars[i]->index = i + 1;
 		vars[i]->data = 0;
 		}
-	cout << "ok" << endl;
 
 	varB.push_back(vars[3]);
 	varB.push_back(vars[4]);
@@ -488,11 +531,26 @@ int main()
 	varN.push_back(vars[0]);
 	varN.push_back(vars[1]);
 	varN.push_back(vars[2]);
+	
+	cout << "find an initial soln:" << endl;
 
+	cout << "varB:" << endl;
+	for (int i=0;i<n;i++)
+	{
+		cout << "varB" << i + 1 << ":" << "var" << varB[i]->index<<"     ";
+	}
+	cout << endl;
+	cout << "varN:" << endl;
+	for (int i = 0; i<m-n; i++)
+	{
+		cout << "varN" << i + 1 << ":" << "var" << varN[i]->index << "     ";
+	}
+	cout << endl;
 	cout << "start interations!!!" << endl;
 //%%%%%%%迭代求解
 	int d = 1;
 	solve(d, varB, c, A, b, varN,vars);
+	cout << endl;
 	cout << "finish!!!" << endl;
 	//print();
 	return 0;
